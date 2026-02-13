45eceacf64206c540d7d34f8a7d174f1fa7ac1cb
SimonasArliukas <simonas.arliukas@gmail.com>
2026-02-13 14:14:24 +0200

    Plotted time series and differenced it for stationarity

---
+++ Macro_factors_effecting_GDP/Src/Feature_selection.py
@@ -0,0 +1,19 @@
+from Src.Data_utils import log_differencing
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import os
+import pandas as pd
+
+load_dotenv()
+##Connect to sql database
+engine = create_engine(
+    f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@"
+    f"{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB')}"
+)
+
+data = pd.read_sql("SELECT * FROM GDP_inference_clean", engine)
+
+
+log_differencing(data)
+
+
