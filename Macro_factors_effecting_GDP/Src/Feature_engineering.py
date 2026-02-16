6d501f0355341bf80e9c363a21633b2d5173d069
SimonasArliukas <simonas.arliukas@gmail.com>
2026-02-16 15:21:52 +0200

    Adding comments in feature engineering

--- Macro_factors_effecting_GDP/Src/Feature_engineering.py
+++ Macro_factors_effecting_GDP/Src/Feature_engineering.py
@@ -23,15 +23,15 @@ def feature_engineering():
     data = pd.read_sql("SELECT * FROM GDP_inference_clean", engine)
 
     data.columns = ["date", "Labor Productivity", "Unemployment Rate", "Federal Funds Rate", "Inflation", "GDP",
-                    "Population", "Investment", "Government Spending"]
-    data["GDP_per_capita"] = data["GDP"]*1000000 / data["Population"]
+                    "Population", "Investment", "Government Spending"] ##Renaming the columns
+    data["GDP_per_capita"] = data["GDP"]*1000000 / data["Population"] ##Rescaling GDP so that GDP and population are both in thousands
 
-    exclude = ["date", "Unemployment Rate", "Federal Funds Rate", "GDP", "Population"]
+    exclude = ["date", "Unemployment Rate", "Federal Funds Rate", "GDP", "Population"] ##Excluding I(0) and redundant variables
     data_i1 = data.loc[:, ~data.columns.isin(exclude)]
 
-    re_order_cols = ['Government Spending', 'Investment', 'Labor Productivity', 'GDP_per_capita', 'Inflation']
+    re_order_cols = ['Government Spending', 'Investment', 'Labor Productivity', 'GDP_per_capita', 'Inflation'] ##Reorder columns for choletsky decomposition
     data_i1_reorder = data_i1[re_order_cols]
-    exogenous_variables = data[["Unemployment Rate", "Federal Funds Rate"]]
+    exogenous_variables = data[["Unemployment Rate", "Federal Funds Rate"]] ##Selecing exogenous variables for VECM
     return data_i1_reorder,exogenous_variables
 
 feature_engineering()
